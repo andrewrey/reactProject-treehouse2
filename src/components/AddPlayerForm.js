2e93df07f7db341d4bbd9645e696b99a2015b5ad
Andrew Reynolds <andrew_reynolds@mac.com>
2020-09-20 12:44:26 -0700

    updated addplayerfrom to user createRef for input value

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,25 +1,18 @@
 import React, { Component } from "react";
 
 class AddPlayerForm extends Component {
-  state = {
-    value: "",
-  };
-  handleValueChange = (e) => {
-    this.setState({ value: e.target.value });
-  };
-
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.addPlayer(this.state.value);
-    this.setState({
-      value: "",
-    });
+    this.props.addPlayer(this.playerInput.current.value);
+    e.currentTarget.reset();
   };
+
+  playerInput = React.createRef();
+
   render() {
-    let { value } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type="text" value={value} onChange={this.handleValueChange} placeholder="Enter a player's name" />
+        <input type="text" ref={this.playerInput} placeholder="Enter a player's name" />
         <input type="submit" value="Add Player" />
       </form>
     );
